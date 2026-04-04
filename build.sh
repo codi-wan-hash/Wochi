36a26d8dd9fd058ee4a2d324ba8b19cfe365eb3f
Stefan <stefanwanner1910@gmail.com>
2026-04-04 22:38:56 +0200

    bugfix - wrong indentation

--- build.sh
+++ build.sh
@@ -15,22 +15,22 @@ if ["$CREATE_SUPERUSER" = "True" ]; then
     echo "Attempting to create superuser..."
 
     python manage.py shell <<EOF
-        import os
-        from django.contrib.auth import get_user_model
+import os
+from django.contrib.auth import get_user_model
 
-        User = get_user_model()
+User = get_user_model()
 
-        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
-        if not username or not email or not password:
-            print("Superuser env vars missing")
-        else:
-            if not User.objects.filter(username=username).exists():
-                User.objects.create_superuser(username=username, email=email, password=password)
-                print(f"Superuser '{username}' created successfully")
-            else:
-                print(f"Superuser '{username}' already exists")
-    EOF
+if not username or not email or not password:
+    print("Superuser env vars missing")
+else:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print(f"Superuser '{username}' created successfully")
+    else:
+        print(f"Superuser '{username}' already exists")
+EOF
 fi
