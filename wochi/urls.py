c18630a2fb3c8ad7c5da33035a1620a6ee0b0f0a
Stefan <stefanwanner1910@gmail.com>
2026-04-05 13:43:03 +0200

    bugfix - wrong first page

--- wochi/urls.py
+++ wochi/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("accounts/", include("accounts.urls")),
+    path("", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("tasks/", include("tasks.urls")),
     path("meals/", include("meals.urls")),
